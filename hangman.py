0cbb8791d09a16d35c3ee3fb0602870c8a594f9f
Jessica Kiroff <jesskiroff@gmail.com>
2024-02-12 14:06:28 -0500

    looping through each letter of word

--- hangman.py
+++ hangman.py
@@ -4,8 +4,10 @@ list_of_words = ["zebra", "lion", "eagle","elephant", "pig", "dinasour",]
 
 word = random.choice(list_of_words)
 
-guess_a_letter = input("guess a letter \n").lower()
+guess_a_letter = input("Guess a letter \n").lower()
 
 for letter in word:
     if letter == guess_a_letter:
         print ("right")
+    else:
+        print("Wrong")
