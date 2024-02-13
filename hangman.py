1409fd3f8ed0354146d1e96421b83fff24c3b2cb
Jessica Kiroff <jesskiroff@gmail.com>
2024-02-13 14:43:29 -0500

    adding while loop to make game continuous

--- hangman.py
+++ hangman.py
@@ -5,20 +5,27 @@ list_of_words = ["zebra", "lion", "eagle","elephant", "pig", "dinasour",]
 word = random.choice(list_of_words)
 print(f'{word}')
 
-guess_a_letter = input("Guess a letter \n").lower()
+end_of_game = False
 
-display = []
+while not end_of_game:
+    guess_a_letter = input("Guess a letter \n").lower()
+
+    display = []
 # for letter in word:
 #     display+= "_"
 
-for lett in range(len(word)):
-    display += "_"
+    for lett in range(len(word)):
+        display += "_"
 
-print(display)
+    print(display)
 
-for position in range(len(word)):
-    letter = word[position]
-    if letter == guess_a_letter:
-        display[position] = letter
+    for position in range(len(word)):
+        letter = word[position]
+        if letter == guess_a_letter:
+            display[position] = letter
 
-print(display)
+    print(display)
+
+    if "_" not in display:
+        end_of_game == True
+        print("You win.")
