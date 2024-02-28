4c7f67d8fcd5cf19052e027c02fb19fb511f4f8f
Jessica Kiroff <jesskiroff@gmail.com>
2024-02-27 19:39:31 -0500

    add function with multiple parameters

--- functions.py
+++ functions.py
@@ -5,9 +5,17 @@
 #This is a build in function
 #How do i build my own?
 
-def my_function(name):
+'''def my_function(name):
     print(f"Hello {name}")
     print(f"and good bye {name}")
 
 my_function("Jessica")
-my_function("Sarah")
+my_function("Sarah") '''
+
+
+#FUNCTION WITH MORE THAN 1 PARAMETER (OR INPUT)
+
+def greet_with(name, location):
+    print(f"Hello {name} from {location}")
+
+greet_with("Jessica", "Tokyo")
