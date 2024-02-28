244616f3bf846ae8a0126ecd6e983543c55dcbcc
Jessica Kiroff <jesskiroff@gmail.com>
2024-02-27 19:35:05 -0500

    add function with parameter and arguments

--- functions.py
+++ functions.py
@@ -5,8 +5,9 @@
 #This is a build in function
 #How do i build my own?
 
-def my_function():
-    print("Hello")
-    print("and good bye")
+def my_function(name):
+    print(f"Hello {name}")
+    print(f"and good bye {name}")
 
-my_function()
+my_function("Jessica")
+my_function("Sarah")
